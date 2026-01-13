5a4551b4275c90636f33e7276c92ffca995cbc81
Tad Keller <tkeller@opentext.com>
2026-01-13 09:51:44 -0600

    remove cluster metadata refresh

--- src/producer_builder.rs
+++ src/producer_builder.rs
@@ -187,15 +187,11 @@ fn into_produce_stream(
 async fn producer<T: BrokerConnection + Clone + Debug + Send + 'static>(
     stream: impl Stream<Item = Vec<ProduceMessage>> + Send + 'static,
     output_sender: UnboundedSender<Vec<Option<ProduceResponse>>>,
-    mut cluster_metadata: ClusterMetadata<T>,
+    cluster_metadata: ClusterMetadata<T>,
     produce_params: ProduceParams,
     attributes: Attributes,
 ) {
     tokio::pin!(stream);
-
-    let mut keepalive_interval = tokio::time::interval(Duration::from_secs(60));
-    keepalive_interval.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Skip);
-
     loop {
         tokio::select! {
             Some(messages) = stream.next() => {
@@ -214,15 +210,6 @@ async fn producer<T: BrokerConnection + Clone + Debug + Send + 'static>(
                         if let Err(err) = output_sender.send(r) {
                             tracing::error!("Error sending results from producer agent {:?}", err);
                         }
-                    }
-                }
-            }
-
-            _ = keepalive_interval.tick() => {
-                if let Some((_id, conn)) = cluster_metadata.broker_connections.iter().next() {
-                    let conn_clone = conn.clone();
-                    if let Err(e) = cluster_metadata.fetch(conn_clone).await {
-                        log::error!("broker metadata refresh failed: {:?}", e);
                     }
                 }
             }
